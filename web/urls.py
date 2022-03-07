b39d1648fb57411a0bad477058a1f422df129fd1
jinu12 <kzkju@naver.com>
2022-03-07 16:06:52 +0900

    UPdate list

--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,9 @@
 from django_request_mapping import UrlPattern
 
 from web.views import MyView
+from web.views_market import MarketView
 
 urlpatterns = UrlPattern()
 urlpatterns.register(MyView)
+urlpatterns.register(MarketView)
+
