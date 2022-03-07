8ae37424e0d1cb1b499ae8ec52a34e81387d061a
jinn722 <jinn722@naver.com>
2022-03-07 17:39:59 +0900

    UPdate views impl

--- web/urls.py
+++ web/urls.py
@@ -2,7 +2,7 @@ from django_request_mapping import UrlPattern
 
 from web.views import MyView
 from web.views_market import MarketView
-from web.views_market import ReviewView
+from web.views_review import ReviewView
 
 urlpatterns = UrlPattern()
 urlpatterns.register(MyView)
