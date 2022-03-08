ec359db2fdbe4ebe236b951218d901362cc18210
jinu12 <kzkju@naver.com>
2022-03-08 14:09:41 +0900

    sawwell_table commit

--- erd/sawwell_table.sql
+++ erd/sawwell_table.sql
@@ -1,20 +1,20 @@
 CREATE TABLE `cust` (
-	`custno`	int(10)	NOT NULL,
+	`custno`	int(10) NOT NULL AUTO_INCREMENT PRIMARY key,
 	`id`	char(30)	NULL,
 	`password`	char(30)	NULL,
 	`name`	char(10)	NULL,
 	`address`	char(100)	NULL,
 	`email`	char(100)	NULL,
-	`regdate`	DATE	NULL
+	`regdate`	DATE DEFAULT NULL default CURDATE()
 );
 
 CREATE TABLE `market` (
-	`marketno`	int(10)	NOT NULL,
+	`marketno`	int(10) NOT NULL AUTO_INCREMENT PRIMARY key,
 	`cid`	int(10)	NOT NULL,
 	`foodid`	int(10)	NOT NULL,
 	`marketname`	char(100)	NULL,
 	`marketaddress`	char(100)	NULL,
-	`regdate`	DATE	NULL,
+	`regdate`	DATE	NULL DEFAULT CURDATE(),
 	`open`	TIME	NULL,
 	`close`	TIME	NULL,
 	`holiday`	date	NULL,
@@ -22,28 +22,28 @@ CREATE TABLE `market` (
 );
 
 CREATE TABLE `review` (
-	`reviewno`	int(10)	NOT NULL,
+	`reviewno`	int(10) NOT NULL AUTO_INCREMENT PRIMARY key,
 	`marketno`	int(10)	NOT NULL,
 	`custno`	int(10)	NOT NULL,
 	`content`	char(100)	NULL,
 	`star`	float(10)	NULL,
-	`regdate`	DATE	NULL
+	`regdate`	DATE	DEFAULT NULL default CURDATE()
 );
 
 CREATE TABLE `categori` (
-	`cid`	int(10)	NOT NULL,
+	`cid`	int(10)	NOT NULL AUTO_INCREMENT PRIMARY key,
 	`categoriname`	char(30)	NULL
 );
 
 CREATE TABLE `food` (
-	`foodid`	int(10)	NOT NULL,
+	`foodid`	int(10) NOT NULL AUTO_INCREMENT PRIMARY key,
 	`name`	char(30)	NULL,
 	`price`	int(10)	NULL,
-	`regdate`	DATE	NULL
+	`regdate`	DATE	NULL DEFAULT CURDATE()
 );
 
 CREATE TABLE `ceo` (
-	`ceoid`	int(10)	NOT NULL,
+	`ceoid`	int(10) NOT NULL AUTO_INCREMENT PRIMARY key,
 	`marketno`	int(10)	NOT NULL,
 	`id`	char(100)	NULL,
 	`password`	char(100)	NULL,
@@ -51,39 +51,11 @@ CREATE TABLE `ceo` (
 );
 
 CREATE TABLE `reply` (
-	`replyid`	int(10)	NOT NULL,
+	`replyid`	int(10) NOT NULL AUTO_INCREMENT PRIMARY key,
 	`reviewno`	int(10)	NOT NULL,
 	`ceoid`	int(10)	NOT NULL,
 	`content`	char(100)	NULL,
-	`regdate`	DATE	NULL
-);
-
-ALTER TABLE `cust` ADD CONSTRAINT PRIMARY KEY (
-	`custno`
-);
-
-ALTER TABLE `market` ADD CONSTRAINT PRIMARY KEY (
-	`marketno`
-);
-
-ALTER TABLE `review` ADD CONSTRAINT PRIMARY KEY (
-	`reviewno`
-);
-
-ALTER TABLE `categori` ADD CONSTRAINT PRIMARY KEY (
-	`cid`
-);
-
-ALTER TABLE `food` ADD CONSTRAINT PRIMARY KEY (
-	`foodid`
-);
-
-ALTER TABLE `ceo` ADD CONSTRAINT PRIMARY KEY (
-	`ceoid`
-);
-
-ALTER TABLE `reply` ADD CONSTRAINT PRIMARY KEY (
-	`replyid`
+	`regdate`	DATE	DEFAULT NULL DEFAULT CURDATE()
 );
 
 ALTER TABLE `market` ADD CONSTRAINT `FK_categori_TO_market_1` FOREIGN KEY (
@@ -134,4 +106,3 @@ ALTER TABLE `reply` ADD CONSTRAINT `FK_ceo_TO_reply_1` FOREIGN KEY (
 REFERENCES `ceo` (
 	`ceoid`
 );
-
