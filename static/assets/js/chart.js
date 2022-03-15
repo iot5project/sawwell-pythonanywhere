da30c204e8a5851186e959798e98b31d63b6c272
JinU(지누) <73889507+jinu12@users.noreply.github.com>
2022-03-15 14:57:49 +0900

    Update chart.js and view menu.html

--- static/assets/js/chart.js
+++ static/assets/js/chart.js
@@ -27,48 +27,48 @@ function display(data){
                     }
                 }
             },
-            series: data
-        });
-      };
+        }]
+    }]
+});
 
-      function getdata(){
-               var data= [{
-                name: 'Brands',
-                colorByPoint: true,
-                data: [{
-                    name: 'Chrome',
-                    y: 61.41,
-                    sliced: true,
-                    selected: true
-                }, {
-                    name: 'Internet Explorer',
-                    y: 11.84
-                }, {
-                    name: 'Firefox',
-                    y: 10.85
-                }, {
-                    name: 'Edge',
-                    y: 4.67
-                }, {
-                    name: 'Safari',
-                    y: 4.18
-                }, {
-                    name: 'Sogou Explorer',
-                    y: 1.64
-                }, {
-                    name: 'Opera',
-                    y: 1.6
-                }, {
-                    name: 'QQ',
-                    y: 1.2
-                }, {
-                    name: 'Other',
-                    y: 2.61
-                }]
-            }]
-            display(data);
-         };
+function getdata(){
+       var data= [{
+        name: 'Brands',
+        colorByPoint: true,
+        data: [{
+            name: 'Chrome',
+            y: 61.41,
+            sliced: true,
+            selected: true
+        }, {
+            name: 'Internet Explorer',
+            y: 11.84
+        }, {
+            name: 'Firefox',
+            y: 10.85
+        }, {
+            name: 'Edge',
+            y: 4.67
+        }, {
+            name: 'Safari',
+            y: 4.18
+        }, {
+            name: 'Sogou Explorer',
+            y: 1.64
+        }, {
+            name: 'Opera',
+            y: 1.6
+        }, {
+            name: 'QQ',
+            y: 1.2
+        }, {
+            name: 'Other',
+            y: 2.61
+        }]
+    }]
+    display(data);
+ };
 
-         $(document).ready(function(){
-            getdata();
-      });
+ $(document).ready(function(){
+    getdata();
+ });
